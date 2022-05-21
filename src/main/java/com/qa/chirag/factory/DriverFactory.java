f02f47058ddc2bd60b906fc4020a72d1762c4198
Chirag Mangukia <chirag.mangukia@gmail.com>
2022-05-21 20:35:35 +0530

    call Enums in the method

--- src/main/java/com/qa/chirag/factory/DriverFactory.java
+++ src/main/java/com/qa/chirag/factory/DriverFactory.java
@@ -3,6 +3,8 @@ package com.qa.chirag.factory;
 import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
+
+import com.qa.chirag.utils.Browsers;
 import com.qa.chirag.utils.OptionsManager;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -13,13 +15,12 @@ public class DriverFactory {
 	OptionsManager optionsManager;
 
 	public WebDriver initDriver(Properties prop) {
-
 		if (driver == null) {
 			optionsManager = new OptionsManager(prop);
 			String browser = prop.getProperty("browser").trim();
-			if (browser.equalsIgnoreCase("chrome")) {
+			if (browser.equalsIgnoreCase(Browsers.CHROME.toString())) {
 				driver = WebDriverManager.chromedriver().capabilities(optionsManager.getChromeOptions()).create();
-			} else if (browser.equalsIgnoreCase("firefox")) {
+			} else if (browser.equalsIgnoreCase(Browsers.FIREFOX.toString())) {
 				driver = WebDriverManager.firefoxdriver().capabilities(optionsManager.getFirefoxOptions()).create();
 			}
 		}
