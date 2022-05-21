14a2eee2ed82668c0483c7311f8fda024624bd86
Chirag Mangukia <chirag.mangukia@gmail.com>
2022-05-21 20:09:47 +0530

    call Constant variables

--- src/test/java/com/qa/chirag/testcases/HomePageTest.java
+++ src/test/java/com/qa/chirag/testcases/HomePageTest.java
@@ -4,19 +4,20 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.qa.chirag.base.BaseTest;
+import com.qa.chirag.utils.Constants;
 
 public class HomePageTest extends BaseTest {
 
 	@Test(priority = 1)
 	public void titleTest() {
 		String actualTitle = homePage.getPageTitle();
-		Assert.assertEquals(actualTitle, "Google");
+		Assert.assertEquals(actualTitle, Constants.HOME_PAGE_TITLE);
 	}
 
 	@Test(priority = 2)
 	public void urlTest() {
 		String actualURL = homePage.getCurrentURL();
-		Assert.assertEquals(actualURL, "https://www.google.com/");
+		Assert.assertEquals(actualURL, Constants.HOME_PAGE_URL_FRACTION);
 	}
 
 	@Test(priority = 3)
