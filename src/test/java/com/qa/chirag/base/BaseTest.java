87d84cc02860ae740df29800bba3297b2cff3869
Chirag Mangukia <chirag.mangukia@gmail.com>
2022-05-21 18:54:55 +0530

    add Basetest class

--- src/test/java/com/qa/chirag/base/BaseTest.java
+++ src/test/java/com/qa/chirag/base/BaseTest.java
@@ -1,5 +1,26 @@
 package com.qa.chirag.base;
 
-public class BaseTest {
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 
+import com.qa.chirag.factory.DriverFactory;
+import com.qa.chirag.utils.Utilities;
+
+public class BaseTest {
+	
+	DriverFactory df;
+	WebDriver driver;
+
+	@BeforeTest
+	public void setup() {
+		df = new DriverFactory();
+		driver = df.initDriver(Utilities.getProp());
+	}
+	
+	@AfterTest
+	public void tearDown() {
+		driver = null;
+		driver.quit();
+	}
 }
