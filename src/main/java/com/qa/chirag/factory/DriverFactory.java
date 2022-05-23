ba52328a106f8aa94cf31ebcf8ec6754df8de2b6
Chirag Mangukia <chirag.mangukia@gmail.com>
2022-05-23 21:13:49 +0530

    Moved takeScreenshot method to DriverFactory class

--- src/main/java/com/qa/chirag/factory/DriverFactory.java
+++ src/main/java/com/qa/chirag/factory/DriverFactory.java
@@ -1,9 +1,14 @@
 package com.qa.chirag.factory;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Properties;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 
+import com.google.common.io.Files;
 import com.qa.chirag.utils.Browsers;
 import com.qa.chirag.utils.OptionsManager;
 
@@ -11,7 +16,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class DriverFactory {
 
-	protected WebDriver driver = null;
+	protected static WebDriver driver = null;
 	OptionsManager optionsManager;
 
 	public WebDriver initDriver(Properties prop) {
@@ -28,6 +33,26 @@ public class DriverFactory {
 		driver.manage().window().maximize();
 		driver.get(prop.getProperty("url").trim());
 		return driver;
+	}
+	
+	/**
+	 *
+	 * @param driver Accepts WebDriver object
+	 * @return Screenshots are stored in Screenshot folder at the root directory.
+	 *         Saved file name is returned.
+	 */
+	public static String takeScreenshot() {
+		String fileName = "";
+		try {
+			File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+			fileName = System.currentTimeMillis() + ".png";
+			String path = "./Screenshots/" + fileName;
+			File destination = new File(path);
+			Files.copy(src, destination);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return fileName;
 	}
 
 }
--- src/main/java/com/qa/chirag/utils/Utilities.java
+++ src/main/java/com/qa/chirag/utils/Utilities.java
@@ -1,16 +1,9 @@
 package com.qa.chirag.utils;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
-
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
-
-import com.google.common.io.Files;
 
 public class Utilities {
 
@@ -28,26 +21,6 @@ public class Utilities {
 			e.printStackTrace();
 		}
 		return prop;
-	}
-
-	/**
-	 *
-	 * @param driver Accepts WebDriver object
-	 * @return Screenshots are stored in Screenshot folder at the root directory.
-	 *         Saved file name is returned.
-	 */
-	public static String takeScreenshot(WebDriver driver) {
-		String fileName = "";
-		try {
-			File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-			fileName = System.currentTimeMillis() + ".png";
-			String path = "./Screenshots/" + fileName;
-			File destination = new File(path);
-			Files.copy(src, destination);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return fileName;
 	}
 
 }
