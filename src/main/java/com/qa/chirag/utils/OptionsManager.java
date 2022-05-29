14425801ee54c9bb75d294f44697cc059fc93682
Chirag Mangukia <chirag.mangukia@gmail.com>
2022-05-29 21:14:32 +0530

    misc

--- src/main/java/com/qa/chirag/utils/OptionsManager.java
+++ src/main/java/com/qa/chirag/utils/OptionsManager.java
@@ -2,7 +2,6 @@ package com.qa.chirag.utils;
 
 import java.util.Properties;
 
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxOptions;
 
@@ -33,41 +32,4 @@ public class OptionsManager {
 			firefox.addArguments("--incognito");
 		return firefox;
 	}
-
-	public WebDriver initDriver(Properties prop) {
-			if (driver == null) {
-				optionsManager = new OptionsManager(prop);
-				String browser = prop.getProperty("browser").trim();
-				if (browser.equalsIgnoreCase(Browsers.CHROME)) {
-					driver = WebDriverManager.chromedriver().capabilities(optionsManager.getChromeOptions()).create();
-				} else if (browser.equalsIgnoreCase(Browsers.FIREFOX)) {
-					driver = WebDriverManager.firefoxdriver().capabilities(optionsManager.getFirefoxOptions()).create();
-				}
-			}
-			driver.manage().deleteAllCookies();
-			driver.manage().window().maximize();
-			driver.get(prop.getProperty("url").trim());
-			return driver;
-		}
-	
-		/**
-		 *
-		 * @param driver Accepts WebDriver object
-		 * @return Screenshots are stored in Screenshot folder at the root directory.
-		 *         Saved file name is returned.
-		 */
-		public static String takeScreenshot() {
-			String fileName = "";
-			try {
-				File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-				fileName = System.currentTimeMillis() + ".png";
-				String path = "./Screenshots/" + fileName;
-				File destination = new File(path);
-				Files.copy(src, destination);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			return fileName;
-		}
-
 }
