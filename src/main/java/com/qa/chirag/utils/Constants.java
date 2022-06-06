0a6b4a845d8b6bb2d6a3c902ef0f06fe87333d79
Chirag Mangukia <chirag.mangukia@gmail.com>
2022-06-06 20:30:06 +0530

    Corrected HomePage Title

--- src/main/java/com/qa/chirag/utils/Constants.java
+++ src/main/java/com/qa/chirag/utils/Constants.java
@@ -3,7 +3,7 @@ package com.qa.chirag.utils;
 public class Constants {
 
 	public static final String HOME_PAGE_TITLE = "Your Store";
-	public static final String HOME_PAGE_URL_FRACTION = "https://naveenautomationlabs.com/opencart/";
+	public static final String HOME_PAGE_URL_FRACTION = "https://naveenautomationlabs.com/opencart/index.php?route=common/home";
 	public static final String LOGIN_PAGE_TITLE = "Account Login";
 	public static final String ACCOUNT_PAGE_TITLE = "My Account";
 	public static final String DESKTOPS_PAGE_TITLE = "Desktops";
