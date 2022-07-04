2070468b7ef872d30bbad6c33185e2a5be17eb3d
Chirag Mangukia <chirag.mangukia@gmail.com>
2022-07-04 22:47:29 +0530

    Removed Hard coded values and implemented Interface for constants

---
+++ src/main/java/com/qa/chirag/utils/Environment.java
@@ -0,0 +1,13 @@
+package com.qa.chirag.utils;
+
+public interface Environment {
+	
+	public static final String QA = "qa";
+	
+	public static final String DEV = "dev";
+	
+	public static final String UAT = "uat";
+	
+	public static final String PROD = "prod";
+	
+}
